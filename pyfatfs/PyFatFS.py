70572bf5e472fcb41b64355b2136e59de6a27527
Milan Hauth <milahu@gmail.com>
2025-08-07 09:16:49 +0200

    copy pyfilesystem2/fs/info.py

--- pyfatfs/PyFatFS.py
+++ pyfatfs/PyFatFS.py
@@ -12,7 +12,7 @@ from fsspec import AbstractFileSystem
 from pyfatfs.mode import Mode
 from pyfatfs.path import split, normpath
 from pyfatfs.permissions import Permissions
-from fsspec.info import Info
+from pyfatfs.info import Info
 from fsspec.errors import DirectoryExpected, DirectoryExists, \
     ResourceNotFound, FileExpected, DirectoryNotEmpty, RemoveRootError, \
     FileExists
