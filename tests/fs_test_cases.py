7e0774708b3073b5c0e9bfb9c09e740e01b9fa6a
Milan Hauth <milahu@gmail.com>
2025-08-07 18:33:53 +0200

    rm test_filterdir

--- tests/fs_test_cases.py
+++ tests/fs_test_cases.py
@@ -1395,64 +1395,6 @@ class FSTestCases(object):
         paged = {r.name for r in itertools.chain(page1, page2)}
         self.assertEqual(paged, {"foo", "bar", "dir"})
 
-    def test_filterdir(self):
-        self.assertEqual(list(self.fs.filterdir("/", files=["*.py"])), [])
-
-        self.fs.makedir("bar")
-        self.fs._create("foo.txt")
-        self.fs._create("foo.py")
-        self.fs._create("foo.pyc")
-
-        page1 = list(self.fs.filterdir("/", page=(None, 2)))
-        page2 = list(self.fs.filterdir("/", page=(2, 4)))
-        page3 = list(self.fs.filterdir("/", page=(4, 6)))
-
-        self.assertEqual(len(page1), 2)
-        self.assertEqual(len(page2), 2)
-        self.assertEqual(len(page3), 0)
-        names = [info.name for info in itertools.chain(page1, page2, page3)]
-        self.assertEqual(set(names), {"foo.txt", "foo.py", "foo.pyc", "bar"})
-
-        # Check filtering by wildcard
-        dir_list = [info.name for info in self.fs.filterdir("/", files=["*.py"])]
-        self.assertEqual(set(dir_list), {"bar", "foo.py"})
-
-        # Check filtering by miltiple wildcard
-        dir_list = [
-            info.name for info in self.fs.filterdir("/", files=["*.py", "*.pyc"])
-        ]
-        self.assertEqual(set(dir_list), {"bar", "foo.py", "foo.pyc"})
-
-        # Check excluding dirs
-        dir_list = [
-            info.name
-            for info in self.fs.filterdir(
-                "/", exclude_dirs=["*"], files=["*.py", "*.pyc"]
-            )
-        ]
-        self.assertEqual(set(dir_list), {"foo.py", "foo.pyc"})
-
-        # Check excluding files
-        dir_list = [info.name for info in self.fs.filterdir("/", exclude_files=["*"])]
-        self.assertEqual(set(dir_list), {"bar"})
-
-        # Check wildcards must be a list
-        with self.assertRaises(TypeError):
-            dir_list = [info.name for info in self.fs.filterdir("/", files="*.py")]
-
-        self.fs.makedir("baz")
-        dir_list = [
-            info.name
-            for info in self.fs.filterdir("/", exclude_files=["*"], dirs=["??z"])
-        ]
-        self.assertEqual(set(dir_list), {"baz"})
-
-        with self.assertRaises(TypeError):
-            dir_list = [
-                info.name
-                for info in self.fs.filterdir("/", exclude_files=["*"], dirs="*.py")
-            ]
-
     def test_readbytes(self):
         # Test readbytes method.
         all_bytes = b"".join(six.int2byte(n) for n in range(256))
